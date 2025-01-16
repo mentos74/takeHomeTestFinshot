562b6633338b7700aa0318ea965659d21e68dc21
mentos74 <rezza.sr@gmail.com>
2025-01-16 09:36:31 +0700

    fix mandatory

--- src/main/java/com/finshot/takehometest/dto/PostCreateRequestDTO.java
+++ src/main/java/com/finshot/takehometest/dto/PostCreateRequestDTO.java
@@ -2,6 +2,7 @@ package com.finshot.takehometest.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,7 +13,11 @@ public class PostCreateRequestDTO {
 
     private Long postId;
 
+    @NotBlank(message = "Title tidak boleh kosong")
     private String title;
+
+    @NotBlank(message = "Password tidak boleh kosong")
+    private String password;
 
     private String author;
 
@@ -24,6 +29,5 @@ public class PostCreateRequestDTO {
 
     private LocalDateTime modifiedAt;
 
-    private String password;
 
 }
